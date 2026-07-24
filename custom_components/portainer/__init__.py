646bdbb091b20be77a6787684835ad1c336a81b6
Administrator <zephyrus0205@gmail.com>
2026-07-24 15:31:23 +0900

    Update 6 files
    - /custom_components/portainer/manifest.json
    - /custom_components/portainer/const.py
    - /custom_components/portainer/__init__.py
    - /.gitignore
    - /README.md
    - /hacs.json

---
+++ custom_components/portainer/__init__.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+PLATFORMS = [
+    Platform.SENSOR,
+    Platform.BINARY_SENSOR,
+    Platform.BUTTON,
+]
+
+
+async def async_setup(
+    hass: HomeAssistant,
+    config: dict,
+) -> bool:
+    return True
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+
+    await hass.config_entries.async_forward_entry_setups(
+        entry,
+        PLATFORMS,
+    )
+
+    return True
+
+
+async def async_unload_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+
+    return await hass.config_entries.async_unload_platforms(
+        entry,
+        PLATFORMS,
+    )
---
+++ custom_components/portainer/const.py
@@ -0,0 +1,12 @@
+DOMAIN = "portainer"
+
+DEFAULT_SCAN_INTERVAL = 30
+
+CONF_API_KEY = "api_key"
+CONF_BASE_URL = "base_url"
+
+PLATFORMS = [
+    "sensor",
+    "binary_sensor",
+    "button",
+]
